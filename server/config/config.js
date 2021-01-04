e793e2dd78d13f40a1124aa698304a2a00ae5d8c
Juan Lima <juanlimarod@gmail.com>
2021-01-04 15:56:56 -0300

    variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -1,20 +1,24 @@
 
 
-
-// Puerto
-
+/*  ==============
+        Puerto
+    ==============  */  
 process.env.PORT = process.env.PORT || 3000;
 
-// Entorno
+/*  ==============
+    Entorno
+    ==============  */
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// Base de Datos
+/*  ==============
+    Base de Datos
+    ==============  */
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/';
 } else {
-    urlDB = 'mongodb://juanlr07:VPdeig0ecqXt7jif@'
+    urlDB = process.env.MONGO_URI
         + 'cluster0-shard-00-00.tmj2e.mongodb.net:27017'
         + 'cluster0-shard-00-01.tmj2e.mongodb.net:27017'
         + 'cluster0-shard-00-02.tmj2e.mongodb.net:27017'
